8ee8b2ec37d1070f3af780520c66d0b0c3409935
ide <as.bestinclass@gmail.com>
2019-11-06 21:42:56 -0600

    message

--- app/src/main/java/com/example/mad/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/mad/DisplayMessageActivity.kt
@@ -11,11 +11,61 @@ class DisplayMessageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_display_message)
 
         // Get the Intent that started this activity and extract the string
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val message = intent.getStringExtra("adj1X")
 
         // Capture the layout's TextView and set the string as its text
-        val textView = findViewById<TextView>(R.id.textView14).apply {
+        val textView = findViewById<TextView>(R.id.textView6).apply {
             text = message
+        }
+
+        val message2 = intent.getStringExtra("adj2X")
+        val textView2 = findViewById<TextView>(R.id.textView14).apply {
+            text = message2
+        }
+
+        val message3 = intent.getStringExtra("adj3X")
+        val textView3 = findViewById<TextView>(R.id.textView31).apply {
+            text = message3
+        }
+
+        val message4 = intent.getStringExtra("advX")
+        val textView4 = findViewById<TextView>(R.id.textView33).apply {
+            text = message4
+        }
+
+        val message5 = intent.getStringExtra("Noun1X")
+        val textView5 = findViewById<TextView>(R.id.textView9).apply {
+            text = message5
+        }
+
+        val message6 = intent.getStringExtra("Noun2X")
+        val textView6 = findViewById<TextView>(R.id.textView12).apply {
+            text = message6
+        }
+
+        val message7 = intent.getStringExtra("Noun3X")
+        val textView7 = findViewById<TextView>(R.id.textView17).apply {
+            text = message7
+        }
+
+        val message8 = intent.getStringExtra("Noun4X")
+        val textView8 = findViewById<TextView>(R.id.textView21).apply {
+            text = message8
+        }
+
+        val message9 = intent.getStringExtra("Noun5X")
+        val textView9 = findViewById<TextView>(R.id.textView27).apply {
+            text = message9
+        }
+
+        val message10 = intent.getStringExtra("Noun6X")
+        val textView10 = findViewById<TextView>(R.id.textView37).apply {
+            text = message10
+        }
+
+        val message11 = intent.getStringExtra("pluralNounX")
+        val textView11 = findViewById<TextView>(R.id.textView39).apply {
+            text = message11
         }
     }
 
--- app/src/main/java/com/example/mad/MainActivity.kt
+++ app/src/main/java/com/example/mad/MainActivity.kt
@@ -6,7 +6,17 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 
-const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+val adj1X = "com"
+val adj2X = "com"
+val adj3X = "com"
+val advX = "com"
+val Noun1X = "com"
+val Noun2X = "com"
+val Noun3X = "com"
+val Noun4X = "com"
+val Noun5X = "com"
+val Noun6X = "com"
+val pluralNounX = "com"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +26,51 @@ class MainActivity : AppCompatActivity() {
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
-        val editText = findViewById<EditText>(R.id.adj1)
-        val message = editText.text.toString()
+        val editText1 = findViewById<EditText>(R.id.adj1)
+        val adj1XF = editText1.text.toString()
+
+        val editText2 = findViewById<EditText>(R.id.adj2)
+        val adj2XF = editText2.text.toString()
+
+        val editText3 = findViewById<EditText>(R.id.adj3)
+        val adj3XF = editText3.text.toString()
+
+        val editText4 = findViewById<EditText>(R.id.adverb)
+        val advXF = editText4.text.toString()
+
+        val editText5 = findViewById<EditText>(R.id.noun1)
+        val noun1XF = editText5.text.toString()
+
+        val editText6 = findViewById<EditText>(R.id.noun2)
+        val noun2XF = editText6.text.toString()
+
+        val editText7 = findViewById<EditText>(R.id.noun3)
+        val noun3XF = editText7.text.toString()
+
+        val editText8 = findViewById<EditText>(R.id.noun4)
+        val noun4XF = editText8.text.toString()
+
+        val editText9 = findViewById<EditText>(R.id.noun5)
+        val noun5XF = editText9.text.toString()
+
+        val editText10 = findViewById<EditText>(R.id.noun6)
+        val noun6XF = editText10.text.toString()
+
+        val editText11 = findViewById<EditText>(R.id.plural_noun)
+        val pluralNounXF = editText11.text.toString()
+
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
+            putExtra("adj1X", adj1XF)
+            putExtra("adj2X", adj2XF)
+            putExtra("adj3X", adj3XF)
+            putExtra("advX", advXF)
+            putExtra("Noun1X", noun1XF)
+            putExtra("Noun2X", noun2XF)
+            putExtra("Noun3X", noun3XF)
+            putExtra("Noun4X", noun4XF)
+            putExtra("Noun5X", noun5XF)
+            putExtra("Noun6X", noun6XF)
+            putExtra("pluralNounX", pluralNounXF)
         }
         startActivity(intent)
     }
